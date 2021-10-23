814bfe638640dd2342335a20f54cc03df5839748
p93d <pat.rob.don@gmail.com>
2021-10-23 16:27:35 -0500

    Initial test deploy
    deploy initial version so i can send to people and view in mobile

--- app.py
+++ app.py
@@ -7,56 +7,127 @@ import pandas as pd
 
 
 """
-matchups = [
-    [home team, away team],
-    etc
-]
+format = seed: [team id, league id, team/manager name]
 """
+teams = {
+    'NFC #1': ['1656657', '326370', 'Patrick Donovan'],
+    'NFC #2': ['1656714', '326382', 'Derek Hubbard'],
+    'NFC #3': ['1656918', '326384', 'Parker Jackering'],
+    'NFC #4': ['1656796', '326387', 'Tyler Stuntebeck'],
+    'AFC #1': ['1658135', '326370', 'timmer5500'],
+    'AFC #2': ['1666713', '326387', 'bmweber33'],
+    'AFC #3': ['1656658', '326375', 'MJahnke'],
+    'AFC #4': ['1656921', '326375', 'Trent Davis'],
+}
+
+
 matchups = [
-    ['1656657', '1656714'],
-    ['1656918', '1656796']
+    ['NFC #1', 'NFC #4'],
+    ['NFC #2', 'NFC #3'],
+    ['AFC #1', 'AFC #4'],
+    ['AFC #2', 'AFC #3'],
 ]
 
-owner_to_league = {
-    '1656657': '326370',    #Me
-    '1656714': '326382',    #DHubb
-    '1656918': '326384',    #Parker
-    '1656796': '326387',    #Tyler
-}
 
-team_list = {
-    '1656657': 'PD',
-    '1656714': 'DHubb',
-    '1656918': 'ParkJack',
-    '1656796': 'TylerS',
-}
 
+week = 7
 
 
 
 
 app = Flask(__name__)
 
-@app.route("/tables")
-@app.route("/")
-def home():
+@app.route("/matchups")
+def matchup_info():
 
     dfs = []
+    home_titles = ['n/a']
+    away_titles = ['n/a']
+
+
+    for matchup in matchups:
+        # variables for each team id/league/name
+        # for easier troubleshooting
+        home_team_id = teams[matchup[0]][0]
+        home_team_league_id = teams[matchup[0]][1]
+        home_team_name = teams[matchup[0]][2]
+        home_team_seed = matchup[0]
+
+        away_team_id = teams[matchup[1]][0]
+        away_team_league_id = teams[matchup[1]][1]
+        away_team_name = teams[matchup[1]][2]
+        away_team_seed = matchup[1]
+
+
+        # get weekly rosters for each team in each matchup
+        dfs.append(flea_flicker_tools.create_matchup(
+            home_team = [
+                home_team_id,
+                home_team_league_id,
+                home_team_name,
+                home_team_seed
+            ],
+            away_team = [
+                away_team_id,
+                away_team_league_id,
+                away_team_name,
+                away_team_seed
+            ],
+            week=week
+        )[0]
+        )
+
+        home_titles.append(f"{home_team_name} ({home_team_seed})")
+        away_titles.append(f"{away_team_name} ({away_team_seed})")
+        
+
+
+    return render_template('tables.html', tables=dfs, home_titles=home_titles, away_titles=away_titles)
+
+
+@app.route("/scoreboard")
+@app.route("/")
+def scores():
+
+    dfs = []
+    
 
     for matchup in matchups:
 
+        # variables for each team id/league/name
+        # for easier troubleshooting
+        home_team_id = teams[matchup[0]][0]
+        home_team_league_id = teams[matchup[0]][1]
+        home_team_name = teams[matchup[0]][2]
+        home_team_seed = matchup[0]
+
+        away_team_id = teams[matchup[1]][0]
+        away_team_league_id = teams[matchup[1]][1]
+        away_team_name = teams[matchup[1]][2]
+        away_team_seed = matchup[1]
+
+
+        # get weekly rosters for each team in each matchup
         dfs.append(flea_flicker_tools.create_matchup(
             home_team = [
-                matchup[0], owner_to_league[matchup[0]]
+                home_team_id,
+                home_team_league_id,
+                home_team_name,
+                home_team_seed
             ],
             away_team = [
-                matchup[1], owner_to_league[matchup[1]]
+                away_team_id,
+                away_team_league_id,
+                away_team_name,
+                away_team_seed
             ],
-            week = 7
-        ).to_html(index=False))#, classes='table table-striped'))
+            week=week
+        )[1]
+        )
+
     
 
-    return render_template('tables.html',  tables=dfs, titles=['Matchup #1', 'Matchup #1', "Matchup #2"])
+    return render_template('tables.html',  tables=dfs, home_titles=[], away_titles=[])
 
 
 
--- flea_flicker_tools.py
+++ flea_flicker_tools.py
@@ -21,12 +21,14 @@ def get_roster(league_id, team_id, week):
         'Actual': []
     }
 
+    yet_to_play = 0
+
     # go through each starter
     for starter in results['groups'][0]['slots']:
 
         # get the slot name
         if starter['position']['label'] == "RB/WR/TE":
-            roster['Slot'].append('Flx')
+            roster['Slot'].append('Flex')
         elif starter['position']['label'] == "D/ST":
             roster['Slot'].append('DST')
         else:
@@ -40,7 +42,7 @@ def get_roster(league_id, team_id, week):
         # if available, get projected points
         try:
             roster['Projected'].append(
-                starter['leaguePlayer']['viewingProjectedPoints']['value']
+                round(starter['leaguePlayer']['viewingProjectedPoints']['value'], 2)
             )
         except:
             roster['Projected'].append('--')
@@ -48,10 +50,11 @@ def get_roster(league_id, team_id, week):
         # if available, get actual points
         try:
             roster['Actual'].append(
-                starter['leaguePlayer']['viewingActualPoints']['value']
+                round(starter['leaguePlayer']['viewingActualPoints']['value'], 2)
             )
         except:
             roster['Actual'].append('--')
+            yet_to_play+=1
 
     proj_pts = 0
     actual_pts = 0
@@ -86,7 +89,7 @@ def get_roster(league_id, team_id, week):
         # if available, get projected points
         try:
             roster['Projected'].append(
-                starter['leaguePlayer']['viewingProjectedPoints']['value']
+                round(starter['leaguePlayer']['viewingProjectedPoints']['value'], 2)
             )
         except:
             roster['Projected'].append('--')
@@ -94,21 +97,21 @@ def get_roster(league_id, team_id, week):
         # if available, get actual points
         try:
             roster['Actual'].append(
-                starter['leaguePlayer']['viewingActualPoints']['value']
+                round(starter['leaguePlayer']['viewingActualPoints']['value'], 2)
             )
         except:
             roster['Actual'].append('--')
 
 
-    return roster
+    return roster, proj_pts, actual_pts, yet_to_play
 
 
 # create the matchup table
 def create_matchup(home_team, away_team, week):
 
     # get current roster for given week for both teams
-    home_roster = get_roster(home_team[1], home_team[0], week)
-    away_roster = get_roster(away_team[1], away_team[0], week)
+    home_roster, home_proj, home_act, home_yet = get_roster(home_team[1], home_team[0], week)
+    away_roster, away_proj, away_act, away_yet = get_roster(away_team[1], away_team[0], week)
 
     boxscore = {
         'Home_Player': home_roster['Player'],
@@ -120,9 +123,9 @@ def create_matchup(home_team, away_team, week):
         'Away_Player': away_roster['Player'],
     }
 
-    df = pd.DataFrame.from_dict(boxscore)
+    boxscore_df = pd.DataFrame.from_dict(boxscore)
 
-    df.rename(
+    boxscore_df.rename(
         columns={
         'Home_Player':'Player',
         'Home_Projected': 'Proj',
@@ -134,4 +137,24 @@ def create_matchup(home_team, away_team, week):
         inplace=True
     )
 
-    return df
+    scoreboard_df = pd.DataFrame.from_dict(
+        {'Manager': [
+            f"{home_team[2]} ({home_team[3]})",
+            f"{away_team[2]} ({away_team[3]})",
+        ],
+        'Yet to Play': [
+            home_yet,
+            away_yet
+        ],
+        'Score': [
+            home_act,
+            away_act
+        ],
+        'Projected Total': [
+            home_proj,
+            away_proj
+        ]
+        }
+    )
+
+    return boxscore_df.to_html(index=False), scoreboard_df.to_html(index=False)
